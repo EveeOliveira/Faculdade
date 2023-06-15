bcf7d98702a13d01aaba649e68ed53457db94a74
Evelen Oliveira <evelenpinheiro62@gmail.com>
2023-06-15 14:17:32 -0300

    lista 2

--- AEDS I/Lista 2/pesquisa.cpp
+++ AEDS I/Lista 2/pesquisa.cpp
@@ -7,7 +7,8 @@ int main() {
     int idade, cont = 1, m = 0;
     string sexo, olhos, cabelos;
 
-    do{
+    // com a estrutura do-while
+    /*do{
         cout << "Analise da pessoa numero " << cont <<endl;
         cout << "Qual o sexo? [M para masculino/ F para feminino]\n";
         cin >> sexo;
@@ -23,7 +24,45 @@ int main() {
         }
 
         cont++;
-    } while(cont <= 500);
+    } while(cont <= 500);*/
+
+    // com a estrutura while
+    /*while(cont <= 500){
+        cout << "Analise da pessoa numero " << cont <<endl;
+        cout << "Qual o sexo? [M para masculino/ F para feminino]\n";
+        cin >> sexo;
+        cout << "Qual a cor dos olhos? [A para azuis/ V para verdes/ C para castanhos]\n";
+        cin >> olhos;
+        cout << "Qual a cor dos cabelos? [L para louros/ C para castanhos/ P para pretos]\n";
+        cin >> cabelos;
+        cout << "Qual a idade?\n";
+        cin >> idade;
+
+        if (sexo == "F" and idade >= 18 && idade <= 35 and olhos == "V" and cabelos == "L"){
+            m++;
+        }
+
+        cont++;
+    }*/
+
+    // com a estrutura for
+    for(int i = 0; cont <= 500; i++){
+        cout << "Analise da pessoa numero " << cont <<endl;
+        cout << "Qual o sexo? [M para masculino/ F para feminino]\n";
+        cin >> sexo;
+        cout << "Qual a cor dos olhos? [A para azuis/ V para verdes/ C para castanhos]\n";
+        cin >> olhos;
+        cout << "Qual a cor dos cabelos? [L para louros/ C para castanhos/ P para pretos]\n";
+        cin >> cabelos;
+        cout << "Qual a idade?\n";
+        cin >> idade;
+
+        if (sexo == "F" and idade >= 18 && idade <= 35 and olhos == "V" and cabelos == "L"){
+            m++;
+        }
+
+        cont++;
+    }
 
     cout << "A analise nos diz que existe(m) " << m << " pessoa(s) com as caracteristicas necessarias.";
 
